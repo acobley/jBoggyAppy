b0ef541b9680dac15c638e64333d49c2f553cf2f
Andy Cobley <acobley@computing.dundee.ac.uk>
2010-08-04 18:11:32 +0100

    Changed Authors to Author

---
+++ src/uk/ac/dundee/computing/aec/jBloggyAppy/Connectors/Author.java
@@ -0,0 +1,27 @@
+package uk.ac.dundee.computing.aec.jBloggyAppy.Connectors;
+import static me.prettyprint.cassandra.utils.StringUtils.string;
+
+import java.util.List;
+import java.util.Map;
+
+import me.prettyprint.cassandra.service.CassandraClient;
+import me.prettyprint.cassandra.service.CassandraClientPool;
+import me.prettyprint.cassandra.service.CassandraClientPoolFactory;
+import me.prettyprint.cassandra.service.Keyspace;
+import org.apache.cassandra.thrift.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class Author {
+	
+	private int key;
+	private String module;
+	private String description;
+	
+	public Author(){
+	
+	}
+	
+
+}
