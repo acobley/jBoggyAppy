aec956fe2dde849906ae11d25284d0ddd1de538c
Andy Cobley <acobley@computing.dundee.ac.uk>
2010-08-04 19:11:56 +0100

    Added cassandra code, but not adding to Author Store

---
+++ src/uk/ac/dundee/computing/aec/jBloggyAppy/Connectors/AuthorConnector.java
@@ -0,0 +1,112 @@
+package uk.ac.dundee.computing.aec.jBloggyAppy.Connectors;
+import static me.prettyprint.cassandra.utils.StringUtils.string;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import me.prettyprint.cassandra.service.CassandraClient;
+import me.prettyprint.cassandra.service.CassandraClientPool;
+import me.prettyprint.cassandra.service.CassandraClientPoolFactory;
+import me.prettyprint.cassandra.service.Keyspace;
+import me.prettyprint.cassandra.service.PoolExhaustedException;
+
+import org.apache.cassandra.thrift.*;
+
+import java.util.List;
+import java.util.Map;
+
+import uk.ac.dundee.computing.aec.jBloggyAppy.Stores.*;
+public class AuthorConnector {
+	
+	String Host=null;
+	private HashMap hm = new HashMap(); //We'll use this to make sure we only get fields we expect from the DB
+	public AuthorConnector(){
+		hm.put("Email","");
+		hm.put("Twitter","");
+		hm.put("Address", "");
+	
+	}
+	
+	public List<AuthorStore> getAuthors()
+	{
+		List<AuthorStore> Authors= new LinkedList<AuthorStore>();
+		AuthorStore Au=new AuthorStore();
+		CassandraClient client=null;
+		try{
+			Connect();
+		}catch (Exception et){
+			System.out.println("Can't Connect"+et);
+			return null;
+		}
+		
+		try{
+			Keyspace ks = client.getKeyspace("BloggyAppy");
+            //retrieve sample data
+            ColumnParent columnParent = new ColumnParent("Authors");
+
+           
+
+            /**
+             * this effect how many columns we are want to retrieve
+             * also check slicePredicate.setColumn_names(java.util.List<byte[]> column_names)
+             * .setColumn_names(new ArrayList<byte[]>()); no columns retrievied at all
+             */
+            SliceRange columnRange = new SliceRange();
+            columnRange.setCount(100);
+            String start="Email";
+            byte bStart[]=start.getBytes();
+            columnRange.setStart(bStart);
+            columnRange.setFinish(new byte[0]);
+            //effect on columns order
+            columnRange.setReversed(false);
+           //count of max retrieving keys
+            KeyRange keyRange = new KeyRange();
+            keyRange.setStart_key("");
+            keyRange.setEnd_key("");
+            SlicePredicate slicePredicate = new SlicePredicate();
+            slicePredicate.setSlice_range(columnRange);
+            Map<String, List<Column>> map = ks.getRangeSlices(columnParent, slicePredicate, keyRange);
+
+            //printing keys with columns
+            for (String key : map.keySet()) {
+                List<Column> columns = map.get(key);
+                //print key
+                System.out.println(key);
+                for (Column column : columns) {
+                    //print columns with values
+                    System.out.println("\t" + string(column.getName()) + "\t ==\t" + string(column.getValue()));
+                }
+            }
+
+            // This line makes sure that even if the client had failures and recovered, a correct
+            // releaseClient is called, on the up to date client.
+            client = ks.getClient();
+
+		}catch (Exception et){
+			System.out.println("Can't get Authors "+et);
+			return null;
+		}
+		return Authors;
+	}
+	
+	public void setHost(String Host){
+	  this.Host=Host;	
+	}
+	
+	private CassandraClient Connect(String Host) throws IllegalStateException, PoolExhaustedException, Exception{
+		CassandraClientPool pool = CassandraClientPoolFactory.INSTANCE.get();
+        CassandraClient client = pool.borrowClient(Host, 9160);
+        return client;
+	}
+	
+
+	private CassandraClient Connect() throws IllegalStateException, PoolExhaustedException, Exception{
+		CassandraClientPool pool = CassandraClientPoolFactory.INSTANCE.get();
+        CassandraClient client = pool.borrowClient(this.Host, 9160);
+        return client;
+	}
+	
+
+}
---
+++ src/uk/ac/dundee/computing/aec/jBloggyAppy/Stores/AuthorStore.java
@@ -0,0 +1,73 @@
+package uk.ac.dundee.computing.aec.jBloggyAppy.Stores;
+
+public class AuthorStore {
+	
+	private int numPosts;
+	private String Name;
+	private String twitterName;
+	private String emailName;
+	private String bio;
+	private String Address;
+	
+	public AuthorStore(){
+	
+	}
+	
+	
+	public int getnumPosts(){
+		return numPosts;
+	}
+	
+	public String getname(){
+		return Name;
+	}
+	
+	public String gettwitterName(){
+		return twitterName;
+	}
+	
+	public String getemailName(){
+		return emailName;
+	}
+	
+	public String getbio(){
+		return bio;
+	}
+	public String getaddress(){
+		return Address;
+	}
+	
+	//A convenient method to set all variables at once.
+	public void setAll(int numPosts, String Name,String twitterName,String emailName, String bio,String Address){
+		this.Name=Name;
+		this.numPosts=numPosts;
+		this.twitterName=twitterName;
+		this.emailName=emailName;
+		this.bio=bio;
+		this.Address=Address;
+	}
+	
+	public void setnumPosts(int Posts){
+		this.numPosts=Posts;
+	}
+	
+	public void setname(String Name){
+		this.Name=Name;
+	}
+	
+	public void settwitterName(String TwitterName){
+		this.twitterName=TwitterName;
+	}
+	
+	public void setemailName(String Email){
+		this.emailName=Email;
+	}
+	
+	public void setbio(String bio){
+		this.bio=bio;
+	}
+	public void setaddress(String address){
+		this.Address=Address;
+	}
+	
+}
